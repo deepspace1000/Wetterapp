2c364c6a1bd41850f37fa2df29d044022466b136
deepspace1000 <nilsrothe05@icloud.com>
2023-04-14 13:08:41 +0200

    added Navigator

--- Wetter_App/App.js
+++ Wetter_App/App.js
@@ -5,6 +5,7 @@ import SearchScreen from './components/SearchScreen';
 import {NavigationContainer} from "@react-navigation/native";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
