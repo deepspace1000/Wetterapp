b39fb9b8ba292d122ebaac875bfc62669003e670
deepspace1000 <nilsrothe05@icloud.com>
2023-04-19 14:37:16 +0200

    added Navigator

--- Wetter_App/App.js
+++ Wetter_App/App.js
@@ -4,7 +4,7 @@ import SearchScreen from './components/SearchScreen';
 import {NavigationContainer} from "@react-navigation/native";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
+import {ImageBackground, View, StyleSheet} from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,31 +14,43 @@ const sucheName = "Suche";
 
 export default function App() {
   return (
-      <NavigationContainer>
-        <Tab.Navigator initialRouteName={wetterName}
-                       screenOptions={({ route }) => ({
-                           tabBarIcon: ({ focused, color, size }) => {
-                               let iconName;
-                               let rn = route.name;
+      <View style={{flex: 1}}>
+          <ImageBackground source={require('./assets/wolke.jpg')} style={styles.background}>
+              <NavigationContainer>
+                <Tab.Navigator initialRouteName={wetterName}
+                               screenOptions={({ route }) => ({
+                                   tabBarIcon: ({ focused, color, size }) => {
+                                       let iconName;
+                                       let rn = route.name;
 
-                               if (rn === wetterName) {
-                                   iconName = focused ? 'cloud' : 'cloud-outline';
+                                       if (rn === wetterName) {
+                                           iconName = focused ? 'cloud' : 'cloud-outline';
 
-                               } else if (rn === favoritenName) {
-                                   iconName = focused ? 'star' : 'star-outline';
+                                       } else if (rn === favoritenName) {
+                                           iconName = focused ? 'star' : 'star-outline';
 
-                               } else if (rn === sucheName) {
-                                   iconName = focused ? 'search' : 'search-outline';
-                               }
+                                       } else if (rn === sucheName) {
+                                           iconName = focused ? 'search' : 'search-outline';
+                                       }
 
 
-                               return <Ionicons name={iconName} size={size} color={color} />;
-                           },
-                       })}>
-          <Tab.Screen name={favoritenName} component={FavoritesScreen}/>
-          <Tab.Screen name={wetterName} component={WeatherScreen}/>
-          <Tab.Screen name={sucheName} component={SearchScreen}/>
-        </Tab.Navigator>
-      </NavigationContainer>
+                                       return <Ionicons name={iconName} size={size} color={color} />;
+                                   },
+                               })}>
+                  <Tab.Screen name={favoritenName} component={FavoritesScreen}/>
+                  <Tab.Screen name={wetterName} component={WeatherScreen}/>
+                  <Tab.Screen name={sucheName} component={SearchScreen}/>
+                </Tab.Navigator>
+              </NavigationContainer>
+          </ImageBackground>
+      </View>
   );
 };
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        //resizeMode: 'cover',
+        //justifyContent: 'center',
+    },
+});
--- Wetter_App/components/WeatherScreen.js
+++ Wetter_App/components/WeatherScreen.js
@@ -4,14 +4,10 @@ import {ImageBackground, View, Text, StyleSheet} from 'react-native';
 
 
 export default function WeatherScreen() {
-    const image = {
-        uri: "https://static.spektrum.de/fm/912/f2000x857/Wolken_Sonne_fotolia_65398541_SunnyForest.jpg",
-    };
+
   return (
     <View style={styles.container}>
-        <ImageBackground source={image} style={styles.background}>
            <Text>Hallo von WeatherScreen!</Text>
-        </ImageBackground>
     </View>
   );
 };
