075483b54a9f7ad043b3cf4908a15ab0339f7231
Ale <alejandro.wenzler@gmail.com>
2023-04-19 10:32:10 +0200

    Icon (Tab-Bar)

--- Wetter_App/App.js
+++ Wetter_App/App.js
@@ -3,17 +3,41 @@ import FavoritesScreen from './components/FavoritesScreen';
 import SearchScreen from './components/SearchScreen';
 import {NavigationContainer} from "@react-navigation/native";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 
 const Tab = createBottomTabNavigator();
 
+const favoritenName = "Favoriten";
+const wetterName = "Wetter";
+const sucheName = "Suche";
+
 export default function App() {
   return (
       <NavigationContainer>
-        <Tab.Navigator initialRouteName={"Wetter"}>
-          <Tab.Screen name="Favoriten" component={FavoritesScreen}/>
-          <Tab.Screen name="Wetter" component={WeatherScreen}/>
-          <Tab.Screen name="Suche" component={SearchScreen}/>
+        <Tab.Navigator initialRouteName={wetterName}
+                       screenOptions={({ route }) => ({
+                           tabBarIcon: ({ focused, color, size }) => {
+                               let iconName;
+                               let rn = route.name;
+
+                               if (rn === wetterName) {
+                                   iconName = focused ? 'cloud' : 'cloud-outline';
+
+                               } else if (rn === favoritenName) {
+                                   iconName = focused ? 'star' : 'star-outline';
+
+                               } else if (rn === sucheName) {
+                                   iconName = focused ? 'search' : 'search-outline';
+                               }
+
+
+                               return <Ionicons name={iconName} size={size} color={color} />;
+                           },
+                       })}>
+          <Tab.Screen name={favoritenName} component={FavoritesScreen}/>
+          <Tab.Screen name={wetterName} component={WeatherScreen}/>
+          <Tab.Screen name={sucheName} component={SearchScreen}/>
         </Tab.Navigator>
       </NavigationContainer>
   );
