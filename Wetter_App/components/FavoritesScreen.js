23a99e9dfb0d48fc345032a194e0b5c5977fd66c
deepspace1000 <nilsrothe05@icloud.com>
2023-04-26 16:37:11 +0200

    Added Local Storage
    Now Stores favorites in persistant storage automaticly

--- Wetter_App/components/FavoritesScreen.js
+++ Wetter_App/components/FavoritesScreen.js
@@ -1,14 +1,34 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, StyleSheet, ImageBackground, FlatList, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, ImageBackground, FlatList, TouchableOpacity, Button} from 'react-native';
 import * as Location from 'expo-location';
-
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 export default function WeatherScreen({navigation}) {
-    const[favoritesList, setFavoritesList] = useState([{id: 0, name: "Waiting"}, {name: 'Zurich'}, {name: 'Bauma'}]);
-
+    const [favoritesList, setFavoritesList] = useState([{key: 0, name: "Waiting.."}]);
     const [locationName, setLocationName] = useState('Waiting..');
+
+    const[value, setValue] = useState([{key: 1, name: "Zurich"}]);
+
+    const storePlace = async () => {
+        try {
+            await AsyncStorage.setItem("locations", JSON.stringify(value));
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const getPlace = async () => {
+        try {
+            const savedPlace = await AsyncStorage.getItem("locations");
+            const currentPlace = JSON.parse(savedPlace);
+            console.log(currentPlace);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    //UseEffect for getting Current Location
     useEffect(() => {
         (async () => {
             let { status } = await Location.requestForegroundPermissionsAsync();
@@ -27,16 +47,17 @@ export default function WeatherScreen({navigation}) {
                 for (let item of response) {
                     let address = `${item.postalCode}, ${item.city}`;
                     setLocationName(address);
-                    console.log(address);
+                    //console.log(address);
                 }
             }
 
         })();
     }, []);
 
+    //Update the Current location with Favorites List
     useEffect(() => {
         const updateCurrentLocation = favoritesList.map((item, i) =>{
-            if (i === 0) {
+            if (item.key === 0) {
                 return {...item, name: locationName };
             }
             else {
@@ -46,21 +67,51 @@ export default function WeatherScreen({navigation}) {
         setFavoritesList(updateCurrentLocation)
     }, [locationName]);
 
+    //useEffect get all saved favorites places
+    useEffect(() => {
+        (async () => {
+            try {
+                const savedPlace = await AsyncStorage.getItem("locations");
+                const currentPlace = JSON.parse(savedPlace);
+                const newList = [...favoritesList, ...currentPlace];
+                setFavoritesList(newList);
+                console.log(currentPlace);
+            } catch (error) {
+                console.log(error)
+            }
+        })();
+    }, []);
+
+    //Update Local storage
+    useEffect(() => {
+        const temp = favoritesList.filter((item) => item.key !== 0);
+        (async () => {
+            try {
+                await AsyncStorage.setItem("locations", JSON.stringify(temp));
+            } catch (error) {
+                console.log(error);
+            }
+        })();
+    }, [favoritesList]);
+
     return (
         <View style={styles.container}>
             <ImageBackground source={require('./../assets/wolke.jpg')} style={styles.background}>
                 <View style={styles.innerContainer}>
+                    <Button title={'save'} onPress={storePlace}></Button>
+                    <Button title={'show'} onPress={getPlace}></Button>
                     <FlatList data={favoritesList}
                               keyExtractor={(x, i) => i}
                               renderItem={({item}) =>
                                   <TouchableOpacity style={styles.listView} onPress={() => {
                                       navigation.navigate('Wetter', {location: item.name,});
                                   }}>
-                                      <Text style={styles.placeName}>{item.name}</Text>
+                                      <Text style={styles.placeName}>key: {item.key}; {item.name}</Text>
                                   </TouchableOpacity>
 
                     }
                     />
+
                 </View>
             </ImageBackground>
         </View>
