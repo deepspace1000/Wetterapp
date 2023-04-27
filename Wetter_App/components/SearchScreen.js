7d11fb32d468ae594655689bdafa1eb9eff723d3
Ale <alejandro.wenzler@gmail.com>
2023-04-27 13:14:49 +0200

    Search Option V0.2

--- Wetter_App/components/SearchScreen.js
+++ Wetter_App/components/SearchScreen.js
@@ -1,58 +1,23 @@
 import React from 'react';
-import {View, Text, StyleSheet, ImageBackground} from 'react-native';
+import {View, Text, StyleSheet, ImageBackground, TextInput} from 'react-native';
 import { useState, useEffect } from 'react';
-import WeatherInfo from "./additionalFiles/WeatherInfo";
+import {Icon} from "@rneui/themed";
 
 
-export default function WeatherScreen() {
+export default function SearchScreen({fetchWeather}) {
 
-    const [weatherdata, setWeatherData] = useState(null);
-    const [loaded, setLoaded] = useState(false);
-
-    const fetchWeather = async (locationName) => {
-        try {
-            setLoaded(false);
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locationName}&appid=80f9a4a554335611a1e77e5b09782627&units=metric`);
-            if(response.status == 200)
-            {
-                const data = await response.json();
-                setWeatherData(data);
-            }
-            else
-            {
-                setWeatherData(null);
-            }
-            setLoaded(true);
-        } catch (error) {
-            console.error("Error");
-        }
-    };
-
-    useEffect(() => {
-        fetchWeather('Zurich');
-    }, []);
-
-    if(!loaded){
-        return(
-            <View style={styles.container}>
-                <Text>Loading</Text>
-            </View>
-        );
-    }
-    else if(weatherdata === null){
-        return(
-            <View style={styles.container}>
-                <Text>City not Found</Text>
-            </View>
-        );
-    }
-
+    const [locationName, setlocationName] = useState('');
 
     return (
         <View style={styles.container}>
             <ImageBackground source={require('./../assets/wolke.jpg')} style={styles.background}>
                 <View>
-                    <WeatherInfo weatherdata={weatherdata} fetchWeather={fetchWeather}/>
+                    <TextInput
+                        placeholder="Geben sie einen Ort ein"
+                        value={locationName}
+                        onChangeText={(text) => setlocationName(text)}
+                    />
+                    <Icon name="search" onPress={() => fetchWeather(locationName)}/>
                 </View>
             </ImageBackground>
         </View>
--- Wetter_App/components/WeatherScreen.js
+++ Wetter_App/components/WeatherScreen.js
@@ -1,18 +1,71 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {ImageBackground, View, Text, StyleSheet} from 'react-native';
 
 
 
+
 export default function WeatherScreen({route}) {
+
+    const [weatherdata, setWeatherData] = useState(null);
+    const [loaded, setLoaded] = useState(false);
+
+    const fetchWeather = async (locationName) => {
+        try {
+            setLoaded(false);
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locationName}&appid=80f9a4a554335611a1e77e5b09782627&units=metric`);
+            if(response.status === 200)
+            {
+                const data = await response.json();
+                setWeatherData(data);
+            }
+            else
+            {
+                setWeatherData(null);
+            }
+            setLoaded(true);
+        } catch (error) {
+            console.error("Error");
+        }
+    };
+
+    useEffect(() => {
+        fetchWeather('Havanna');
+    }, []);
+
+
+
     const{location} = route.params;
+
+
+    if(!loaded){
+        return(
+            <View style={styles.container}>
+                <Text>Loading</Text>
+            </View>
+        );
+    }
+    else if(weatherdata === null) {
+        return (
+            <View style={styles.container}>
+                <Text>City not Found</Text>
+            </View>
+        );
+    }
+
+    const {
+        //Bald werden noch mehr Daten zu auswahl stehen
+        main: {temp}
+    } = weatherdata;
+
+
   return (
     <View style={styles.container}>
         <ImageBackground source={require('./../assets/wolke.jpg')} style={styles.background}>
             <View>
                 <Text>Hallo von WeatherScreen!</Text>
                 <Text>{location}</Text>
+                <Text>{temp}</Text>
             </View>
-
         </ImageBackground>
     </View>
   );
