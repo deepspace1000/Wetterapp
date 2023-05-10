a0be95048143a16864757a43bb363280c6f7ae7f
deepspace1000 <nilsrothe05@icloud.com>
2023-05-10 10:36:28 +0200

    maps added

--- Wetter_App/components/SearchScreen.js
+++ Wetter_App/components/SearchScreen.js
@@ -1,25 +1,58 @@
 import React from 'react';
-import {View, Text, StyleSheet, ImageBackground, TextInput} from 'react-native';
+import {View, Text, StyleSheet, ImageBackground, TextInput, Button} from 'react-native';
 import { useState, useEffect } from 'react';
 import {Icon} from "@rneui/themed";
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+import {geocodeAsync} from "expo-location";
 
 
 
 export default function SearchScreen({navigation}) {
 
     const [locationName, setlocationName] = useState('');
+    const [position, setPosition] = useState(null);
+    const [error, setError] = useState(null);
+
+    const handleGeocode = async () => {
+        try{
+            const response = await Location.geocodeAsync(locationName);
+            setPosition(response[0]);
+            setError(null);
+        } catch (err) {
+            setPosition(null);
+            setError(err.message);
+        }
+    }
+
+    useEffect(()=>{
+        handleGeocode();
+
+    }, [locationName]);
 
     return (
         <View style={styles.container}>
             <ImageBackground source={require('./../assets/wolke.jpg')} style={styles.background}>
-                <View>
-                    <TextInput
-                        placeholder="Geben sie einen Ort ein"
-                        value={locationName}
-                        onChangeText={(text) => setlocationName(text)}
-                    />
-                    <Icon name="search" onPress={() => {
-                        navigation.navigate('Wetter', {location: locationName})}}/>
+                <View style={styles.innerContainer}>
+                    <View style={styles.searchBar}>
+                        <TextInput
+                            style={styles.textInput}
+                            placeholder="Geben sie einen Ort ein"
+                            value={locationName}
+                            onChangeText={(text) => setlocationName(text)}
+                        />
+                        <Icon  name="search" style={styles.searchItem} onPress={() => {
+                            navigation.navigate('Wetter', {location: locationName})}}/>
+                    </View>
+                    <View style={styles.mapView}>
+                        {position ? (
+                            <MapView style={styles.map} region={position}>
+                                <Marker coordinate={position} />
+                            </MapView>
+                        ) : (
+                            <Text>Loading...</Text>
+                        )}
+                    </View>
                 </View>
             </ImageBackground>
         </View>
@@ -34,6 +67,37 @@ const styles = StyleSheet.create({
         flex: 1,
         resizeMode: 'cover',
         justifyContent: 'center',
-        alignItems: "center",
+        alignItems: 'stretch',
+    },
+    innerContainer: {
+        flex: 1,
+        alignItems: 'center',
+        marginTop: 90,
+    },
+    searchBar: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        backgroundColor: '#ffffff',
+        margin: 5,
+        height: 50,
+        borderRadius: 15,
+        width: '60%'
+
+    },
+    textInput: {
+        flex: 1,
+        marginLeft: 10,
+    },
+    searchItem: {
+        alignSelf: "flex-end",
+        marginRight: 20,
+    },
+    mapView: {
+        flex: 1,
+        //alignItems: 'stretch',
+    },
+    map: {
+        height: 300,
+        width: 300,
     },
 });
